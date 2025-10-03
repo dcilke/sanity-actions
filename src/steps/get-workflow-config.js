296a88a747b808b63b2ecc69365e43c66f3d54bb
Dain Cilke <dain.cilke@gmail.com>
2025-10-03 11:54:54 +0200

    fix actions

---
+++ src/steps/get-workflow-config.js
@@ -0,0 +1,16 @@
+import {isPullRequest} from '../lib/github.js'
+import {generateDeploymentId} from '../lib/utils.js'
+
+export function getWorkflowConfig() {
+  if (isPullRequest()) {
+    return {
+      isPR: true,
+      deploymentId: generateDeploymentId(),
+    }
+  }
+
+  return {
+    isPR: false,
+    deploymentId: undefined,
+  }
+}
